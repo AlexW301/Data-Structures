0c81fba6ec8cb555ead22c696ff1a9999e1bf747
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-07-03 08:51:15 -0400

    Update binary-search-tree.js

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -50,6 +50,21 @@ class BST {
         }
         return false
     }
+
+    BFS() {
+    let currentNode = this.root
+    let queue = []
+    let results = []
+    queue.push(currentNode)
+
+    while(queue.length) {
+        currentNode = queue.shift()
+        results.push(currentNode.value)
+        if(currentNode.left) queue.push(currentNode.left)
+        if(currentNode.right) queue.push(currentNode.right)
+    }
+    return results
+}
 }
 
 let myTree = new BST()
